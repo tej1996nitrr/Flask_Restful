0fbb650b4ce0625f8bce990d45423bcac5773549
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2019-09-14 14:19:56 +0530

    added database

--- app.py
+++ app.py
@@ -2,70 +2,73 @@ from flask import Flask,request
 from flask_restful import Resource,Api,reqparse
 from flask_jwt import  JWT,jwt_required
 from security import authenticate,identity
+from user import UserRegister
+from item import Item,ItemList
 
 app=Flask(__name__)
 app.secret_key = 'flask'
 api =Api(app)
 
 jwt = JWT(app,authenticate,identity)
-items=[]
+# items=[]
 #no need to jsonify, flask restful does it. so we pass dict only
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-    @jwt_required
-    def get(self,name):
-        item=next(filter(lambda x:x['name']==name,items),None)
-        #next gives first item matched by the filter
-        #if no item found retrn None
+# class Item(Resource):
+#     parser = reqparse.RequestParser()
+#     parser.add_argument('price',
+#         type=float,
+#         required=True,
+#         help="This field cannot be left blank!"
+#     )
+#     @jwt_required
+#     def get(self,name):
+#         item=next(filter(lambda x:x['name']==name,items),None)
+#         #next gives first item matched by the filter
+#         #if no item found retrn None
 
 
 
-        # for i in items:
-        #     if i['name']==name:
-        #         return i
+#         # for i in items:
+#         #     if i['name']==name:
+#         #         return i
 
-        return {'item':item},200 if item else 404  # 404 for not found
+#         return {'item':item},200 if item else 404  # 404 for not found
 
-    def post(self,name):
-        if next(filter(lambda x:x['name']==name,items),None):
+#     def post(self,name):
+#         if next(filter(lambda x:x['name']==name,items),None):
 
-            return {'message':"An item already exist '{}'".format(name)},400 #bad request
-        #data=request.get_json()
-        data = Item.parser.parse_args()
-        item={'name':name,'price':data['price']}
-        items.append(item)
-        return item ,201    #201 is code for created
+#             return {'message':"An item already exist '{}'".format(name)},400 #bad request
+#         #data=request.get_json()
+#         data = Item.parser.parse_args()
+#         item={'name':name,'price':data['price']}
+#         items.append(item)
+#         return item ,201    #201 is code for created
 
-    def delete(self,name):
-        global items
-        items=list(filter(lambda x:x['name']!=name,items))
-        return {'message':'item Deleted'}
+#     def delete(self,name):
+#         global items
+#         items=list(filter(lambda x:x['name']!=name,items))
+#         return {'message':'item Deleted'}
     
-    def put(self,name):
-        #request is imported from flask
+#     def put(self,name):
+#         #request is imported from flask
         
-        data = Item.parser.parse_args()
-        #data = request.get_json()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
+#         data = Item.parser.parse_args()
+#         #data = request.get_json()
+#         item = next(filter(lambda x: x['name'] == name, items), None)
+#         if item is None:
+#             item = {'name': name, 'price': data['price']}
+#             items.append(item)
+#         else:
+#             item.update(data)
+#         return item
 
 
-class ItemList(Resource):
-    def get(self):
-        return {'items':items}
+# class ItemList(Resource):
+#     def get(self):
+#         return {'items':items}
 
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 
 
